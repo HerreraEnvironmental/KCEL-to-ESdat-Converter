93e136cabd675e76b5046b1e918f79f3a80a81c4
Nikki VandePutte <NVandePutte@herrerainc.com>
2025-08-20 08:17:09 -0700

    updates to TESL converter

--- ESdat-Converter-Tools/supporting-scripts/TESL/ESdat_header.R
+++ ESdat-Converter-Tools/supporting-scripts/TESL/ESdat_header.R
@@ -25,33 +25,21 @@
   temp_dir <- tempdir()
 
 ## Import config.yaml file
-  config        <- read_yaml("ESdat-Converter-Tools/supporting-scripts/KCEL/config.yaml")
+  config        <- read_yaml("ESdat-Converter-Tools/supporting-scripts/TESL/config.yaml")
   proj_num      <- config$project_info$project_number
   proj_ID       <- config$project_info$project_name
 
 ## Import files
   # Raw files
-  files         <- list.files("data/KCEL/data_raw", pattern = "*.csv")
+  files         <- list.files("data/TESL/data_raw", pattern = "*SampleFile*")
   # Lab report names
-  lab_reports   <- substring(files, 1, 6) # !! May change in the future !!
-  # Extract dates and re-format
-  # find_date     <- function(my_files){
-  #   proj_name_str_length        <- as.numeric(nchar(proj_ID))
-  #   position_to_date_start      <- proj_name_str_length + 22 # Num of chars in report name (6) +6 for white space and misc. chars
-  #   position_to_date_end        <- position_to_date_start + 7
-  #   my_dates                    <- substring(my_files, position_to_date_start, position_to_date_end)
-  #   my_dates                    <- gsub(" ", "", my_dates, fixed = T) # Remove whitespace as necessary
-  # }
-
-  # dates         <- find_date(files)
-  # dates         <- substring(files, 24, 30)
-  # dates         <- as.character(dmy(dates))
-  dates           <- as.character(Sys.Date())
+  lab_reports   <- substring(files, 1, 7)
+  
   # Read in secondary files
-  files         <- list.files("data/KCEL/data_secondary")
+  files         <- list.files("data/TESL/data_secondary")
   # Build XML based on project
   for (i in 1:length(lab_reports)){
-    report_date <- dates[i]
+    report_date <- as.character(Sys.Date())
     report_num <- lab_reports[i]
     sample_file <- grep(paste0(report_num, ".ESdatSample"), files, value = TRUE)
     chemistry_file <- grep(paste0(report_num, ".ESdatChemistry"), files, value = TRUE)
@@ -65,8 +53,8 @@
                        generated="2013-05-09T10:20:17+10:00",
                        xmlns="http://www.escis.com.au/2013/XML")              
     report_node = newXMLNode("LabReport", parent=root)
-    xmlAttrs(report_node) = c(Lab_Signatory = "Meghan Elkey",
-                              Lab_Name = "KCEL",
+    xmlAttrs(report_node) = c(Lab_Signatory = "Tiffany Ryan",
+                              Lab_Name = "TESL",
                               Project_Number = proj_num,
                               Project_ID = proj_num,
                               Date_Reported = report_date,
@@ -97,6 +85,6 @@
     # qual_2 = newXMLNode("Lab_Qualifier", parent = qualifier_node)
     # xmlAttrs(qual_2) = c(Description = "The concentration is below the reporting limit", Code = "J")
     
-    saveXML(doc, file = paste0("data/KCEL/data_secondary/", proj_num, ".", report_num, ".ESdatHeader.xml"))
+    saveXML(doc, file = paste0("data/TESL/data_secondary/", proj_num, ".", report_num, ".ESdatHeader.xml"))
     
   }
---
+++ ESdat-Converter-Tools/supporting-scripts/TESL/TESL_to_ESdat_update.R
@@ -0,0 +1,94 @@
+## ---------------------------
+## Script name:         TESL_to_ESdat.R  
+## Purpose of script:   Convert TESL EDDs to ESdat format
+## Author:              N. VandePutte
+## Date Created:        2025-03-07
+## Date Updated:        2025-03-11
+## Project #:           N/A
+## Task # (optional):   N/A
+## ---------------------------
+## Notes: This script is ran in conjunction with the ESdat_prep.R and ESdat_header.R scripts
+##
+##    *** Have you updated the config.yaml file? Please do so before running any scripts ***
+##        
+## ---------------------------
+
+## Required Packages (install as necessary)
+  library(dplyr)
+  library(tidyr)
+  library(readxl)
+  library(lubridate)
+  library(tidyverse)
+  library(XML)
+  library(yaml)
+
+## Import files
+  # Raw files
+  chemistry_files <- list.files("data/TESL/data_raw", full.names = TRUE, pattern = "*ChemistryFile*")
+  sample_files <- list.files("data/TESL/data_raw", full.names = TRUE, pattern = "*SampleFile*")
+  
+  chem_codes <- read.csv("ESdat-Converter-Tools/supporting-scripts/TESL/chem_code_lookup.csv")
+
+## Import config.yaml file
+  config        <- read_yaml("ESdat-Converter-Tools/supporting-scripts/TESL/config.yaml")
+  proj_num      <- config$project_info$project_number
+  proj_ID       <- config$project_info$project_name
+  proj_site     <- config$project_info$project_site
+  locations     <- config$project_info$locations
+  
+  # locations <- read.csv("ESdat_locations.csv") %>%
+  #   filter(Site_ID == proj_site)
+  
+## Retrieve lab report names
+  lab_reports <- substring(list.files("data/TESL/data_raw", pattern = "*ChemistryFile*"), 1, 7) # TODO adjust to TESL report names
+
+## Iterate through lab reports and create Sample and Chemistry CSV files
+  for(i in 1:length(lab_reports)){
+    # Assign lab report
+    lab_report <- lab_reports[i]
+  
+    sample <- read.csv(grep(lab_report, sample_files, value = TRUE))
+    chemistry <- read.csv(grep(lab_report, chemistry_files, value = TRUE))
+  
+  # Sample CSV dataframe building
+    sample <- sample %>%
+      mutate(Sampled_Date_Time = mdy_hms(Sampled_Date_Time),
+             Site_ID = proj_site,
+             Depth = "",
+             Location_Code = ifelse(Field_ID == "", "", 
+                                    str_extract(Field_ID, paste0(locations, collapse = "|"))),
+             Sample_Type = case_when(grepl("DUP", Field_ID) ~ "Field_D", 
+                                     grepl("QA", Field_ID) ~ "Field_B",
+                                     grepl("BLK", Sample_Type) ~ "MB",
+                                     grepl("DUP", Sample_Type) ~ "LAB_D",
+                                     grepl("MS", Sample_Type) ~ "MS",
+                                     grepl("SRM", Sample_Type) ~ "SRM",
+                                     grepl("BS", Sample_Type) ~ "LCS",
+                                     .default = "Normal") # Required
+    )
+    
+  # Export Sample file
+    write.csv(sample, paste0("data/TESL/data_secondary/", proj_num, ".", lab_report, ".ESdatSample.csv"))
+  
+  # Chemistry CSV dataframe building
+    chemistry <- chemistry %>%
+      mutate(Prefix = ifelse(Result == "ND", "<", ""),
+             Result = ifelse(Result == "ND", EQL, Result), # Required
+             Total_or_Filtered = as.character(if_else(Total_or_Filtered == "D", "F", "T")),
+             Extraction_Date = mdy_hms(Extraction_Date),
+             Anaysed_Date = mdy_hms(Analysed_Date),
+             Lab_Analysis_ID = SampleCode,
+             Lab_Qualifier = ifelse(Result == "ND", paste0("U", Lab_Qualifier), Lab_Qualifier)
+             )
+    
+    chemistry <- left_join(chemistry, chem_codes)
+    
+  # Export Chemistry file
+    write.csv(chemistry, paste0("data/TESL/data_secondary/", proj_num, ".", lab_report, ".ESdatChemistry.csv"))
+  }
+  
+## Import PDF lab reports and copy to secondary folder
+  pdfs <- list.files("data/TESL/data_raw", full.names = TRUE, pattern = "*.pdf")
+  for (i in 1:length(pdfs)){
+    file.copy(pdfs[i], paste0("data/TESL/data_secondary/", proj_num, ".", lab_reports[i], ".ESdat.pdf"))
+    }
